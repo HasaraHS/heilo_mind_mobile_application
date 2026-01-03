8e19d3ecdee45a57f32e77cebe2bbe802bf3f8c0
IT22342058 <IT22342058@my.sliit.lk>
2026-01-03 19:19:43 +0530

    propfile with logout function

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -13,16 +13,10 @@ const Home = () => {
   const {user} = useAuth()
 
   console.log("user", user);
-   const handleLogout = async () => {
-      await signOut(auth)
-    }
-
+  
   return (
     <ScreenWrapper>
       <Typo>Home</Typo>
-      <Button onPress={handleLogout}>
-        <Typo color={colors.background}>Logout</Typo>
-      </Button>
     </ScreenWrapper>
   )
 }
