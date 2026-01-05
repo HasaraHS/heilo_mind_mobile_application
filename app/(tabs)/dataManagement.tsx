47b4ccb2c118a0105b1d9b0933e6d3b16bf52c53
IT22342058 <IT22342058@my.sliit.lk>
2026-01-05 07:52:29 +0530

    commit

--- app/(tabs)/dataManagement.tsx
+++ app/(tabs)/dataManagement.tsx
@@ -1,16 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '@/components/ScreenWrapper'
-import Typo from '@/components/Typo'
+import Header from '@/components/Header';
+import { spacingX, spacingY } from '@/constants/theme'
 
 const DataManagement = () => {
   return (
     <ScreenWrapper>
-      <Typo>DataManagement</Typo>
+     <View style={styles.container}>
+      {/* header */}
+        <Header title="Data Synchronization" style={{ marginVertical: spacingY._10 }} />
+     </View>
     </ScreenWrapper>
   )
 }
 
 export default DataManagement
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+     flex:1,
+     paddingHorizontal: spacingX._20,
+  }
+})
